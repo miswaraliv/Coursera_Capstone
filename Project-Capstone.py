49c49e45f5119aa9214aa4fa3ca233847648b145
Miswar Ali VP <66779302+miswaraliv@users.noreply.github.com>
2020-08-06 18:57:27 +0530

    Added a file introducing the Project Capstone

---
+++ Project-Capstone.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ###  Project Capstone for COURSERA
+
+# ### This exercise is to push this notebook to the git repository 
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+import numpy as np
+
+
+# In[ ]:
+
+
+print('Hello Capstone Project ')
+
