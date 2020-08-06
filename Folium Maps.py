1341c4fa9b134945ec74caf6932840749b3dae22
Miswar Ali VP <66779302+miswaraliv@users.noreply.github.com>
2020-08-06 18:44:09 +0530

    New python notebook included which contains Folium Maps

---
+++ Folium Maps.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+## Capstone 
+
+
+# In[3]:
+
+
+import folium from Folium
+
+
+# In[9]:
+
+
+import folium
+
+
+# In[10]:
+
+
+m=folium.Map()
+
+
+# In[11]:
+
+
+m
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
